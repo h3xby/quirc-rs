48c744413283136358355bda98b22f0bb2e463fb
Dmitry Kontsevoy <dmitry.kontsevoy@gmail.com>
2018-06-05 22:52:58 +0300

    update example

--- examples/read.rs
+++ examples/read.rs
@@ -10,12 +10,8 @@ use quirc::QrCoder;
 
 fn main() {
     let arg = args().nth(1).expect("a file argument");
-    let mut file = File::open(arg).unwrap();
 
-    let mut vec = Vec::new();
-    file.read_to_end(&mut vec).unwrap();
-
-    let image = image::load_from_memory(&vec).unwrap().to_luma();
+    let image = image::open(arg).unwrap().to_luma();
 
     let mut quirc = QrCoder::new().unwrap();
 
